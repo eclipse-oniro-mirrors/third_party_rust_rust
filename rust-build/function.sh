4acbbb546297707bd678ded2e28edab2d4093b87
openharmony_ci <120357966@qq.com>
2025-10-02 16:22:20 +0000

    !63 sess: stabilize -Zrelro-level as -Crelro-level Merge pull request !63 from xiaoyu/relro-level

---
+++ rust-build/function.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+
+detect_platform() {
+    case $(uname -s) in
+        Linux) host_platform=linux ;;
+        Darwin) host_platform=darwin ;;
+        *) echo "Unsupported host platform: $(uname -s)"; exit 1 ;;
+    esac
+
+    case $(uname -m) in
+        arm64) host_cpu=arm64 ;;
+        *) host_cpu=x86_64 ;;
+    esac
+}
+
+clean() {
+    rm -rf ${rust_source_dir}/build/*
+    rm -rf ${root_build_dir}/output/*
+    echo "Cleaning up the previous build result is complete"
+}
+
+copy_config() {
+    if [ "${host_platform}" = "linux" ] && [ "${host_cpu}" = "x86_64" ]; then
+        cp ${shell_path}/config.toml ${rust_source_dir}
+        cp ${shell_path}/tools/* ${rust_source_dir}/build/
+        chmod 750 ${rust_source_dir}/build/*clang*
+    elif [ "${host_platform}" = "darwin" ]; then
+        if [ "${host_cpu}" = "x86_64" ]; then
+            cp ${shell_path}/mac_x8664_config.toml ${rust_source_dir}/config.toml
+        else
+            cp ${shell_path}/mac_arm64_config.toml ${rust_source_dir}/config.toml
+        fi
+    else
+        echo "Unsupported platform: $(uname -s) $(uname -m)"
+        exit 1
+    fi
+    echo "Copy config.toml to the source code directory"
+}
+
+update_config_clang_path() {
+    # $1:clang path, $2:clang name, $3:ar name
+
+    # add clang absolute path to shell tools
+    sys_clang_dir="$(echo ${1} | sed 's/\//\\\//g')"
+    sed -i "s/exec ${2}/exec ${sys_clang_dir}\/${2}/g" ${rust_source_dir}/build/${2}
+    sed -i "s/exec ${2}++/exec ${sys_clang_dir}\/${2}++/g" ${rust_source_dir}/build/${2}++
+    # add clang absolute path to config.toml
+    rust_clang_dir="$(echo ${rust_source_dir}/build | sed 's/\//\\\//g')"
+    sed -i "s/cc = \"${2}\"/cc = \"${rust_clang_dir}\/${2}\"/g" ${rust_source_dir}/config.toml
+    sed -i "s/cxx = \"${2}++\"/cxx = \"${rust_clang_dir}\/${2}++\"/g" ${rust_source_dir}/config.toml
+    sed -i "s/linker = \"${2}\"/linker = \"${sys_clang_dir}\/${2}\"/g" ${rust_source_dir}/config.toml
+    sed -i "s/ar = \"${3}\"/ar = \"${sys_clang_dir}\/${3}\"/g" ${rust_source_dir}/config.toml
+}
+
+update_musl_head_file_path() {
+    # $1:musl head file path, $2 clang name
+    # add musl head file path to shell tools
+    musl_head_file_dir="$(echo ${1} | sed 's/\//\\\//g')"
+    sed -i "s/-Imusl/-I${musl_head_file_dir}/g" ${rust_source_dir}/build/${2}
+    sed -i "s/-Imusl/-I${musl_head_file_dir}/g" ${rust_source_dir}/build/${2}++
+}
+
+update_config_clang() {
+    # $1:OH clang path $2:mingw clang path $3:musl head file path
+    if [ "${host_platform}" = "linux" ] && [ ${host_cpu} = "x86_64" ]; then
+        update_config_clang_path ${1} clang llvm-ar
+        update_config_clang_path ${1} aarch64-unknown-linux-ohos-clang llvm-ar
+        update_config_clang_path ${1} armv7-unknown-linux-ohos-clang llvm-ar
+        update_config_clang_path ${1} x86_64-unknown-linux-ohos-clang llvm-ar
+        update_config_clang_path ${2} x86_64-w64-mingw32-clang x86_64-w64-mingw32-ar
+        update_musl_head_file_path ${3} aarch64-unknown-linux-ohos-clang
+        update_musl_head_file_path ${3} armv7-unknown-linux-ohos-clang
+        update_musl_head_file_path ${3} x86_64-unknown-linux-ohos-clang
+    fi
+}
+
+update_version() {
+    if [ "${host_platform}" = "linux" ] && [ ${host_cpu} = "x86_64" ]; then
+        sed -i "s/$old_version/$new_version/g" ${rust_source_dir}/config.toml
+    elif [ "${host_platform}" = "darwin" ] && [ ${host_cpu} = "x86_64" ]; then
+        sed -i "" "s/$old_version/$new_version/g" ${rust_source_dir}/config.toml
+    elif [ "${host_platform}" = "darwin" ] && [ ${host_cpu} = "arm64" ]; then
+        sed -i "" "s/$old_version/$new_version/g" ${rust_source_dir}/config.toml
+    else
+        echo "Unsupported platform: $(uname -s) $(uname -m)"
+    fi
+}
+
+move_static_rust_source() {
+    # $1:static rust code path. $2:rust code path
+    pushd ${1}
+    tar xf rustc-1.72.0-src.tar.gz
+    pushd ${1}/rustc-1.72.0-src
+    cp -r {.cargo,vendor} ${2}
+    cp -r library/{backtrace,stdarch} ${2}/library/
+    cp -r src/doc/{book,edition-guide,embedded-book,nomicon} ${2}/src/doc/
+    cp -r src/doc/{reference,rust-by-example,rustc-dev-guide} ${2}/src/doc/
+    cp -r src/tools/cargo/* ${2}/src/tools/cargo/
+    echo "Copy the static source code completely."
+    popd
+    popd
+}
+
+download_rust_static_source() {
+    local pre_rust_date="2023-07-13"
+    mkdir -p ${rust_static_dir} ${rust_source_dir}/build/cache/${pre_rust_date}
+    local rust_down_net="https://repo.huaweicloud.com/harmonyos/compiler/rust"
+    pushd ${rust_static_dir}
+    local platform=$1
+    local cpu=$2
+    local files=("rust-std-1.71.0-${cpu}-${platform}.tar.xz" "rustc-1.71.0-${cpu}-${platform}.tar.xz" \
+                 "cargo-1.71.0-${cpu}-${platform}.tar.xz")
+
+    for file in "${files[@]}"; do
+        if [ ! -e "${file}" ]; then
+            curl -O -k -m 300 ${rust_down_net}/1.71.0/${file} &
+        fi
+    done
+    wait
+
+    if [ ! -e "rustc-1.72.0-src.tar.gz" ]; then
+            curl -O -k -m 600 ${rust_down_net}/1.72.0/rustc-1.72.0-src.tar.gz
+    fi
+    popd
+    cp ${rust_static_dir}/*.tar.xz ${rust_source_dir}/build/cache/${pre_rust_date}/
+    cp ${rust_static_dir}/rustc-1.72.0-src.tar.gz ${root_build_dir}
+}
+
+download_rust_at_net() {
+    if [ "${host_platform}" = "linux" ] && [ "${host_cpu}" = "x86_64" ]; then
+        download_rust_static_source "unknown-linux-gnu" "x86_64"
+    elif [ "${host_platform}" = "darwin" ]; then
+        if [ "${host_cpu}" = "x86_64" ]; then
+            download_rust_static_source "apple-darwin" "x86_64"
+        else
+            download_rust_static_source "apple-darwin" "aarch64"
+        fi
+    else
+        echo "Unsupported platform: $(uname -s) $(uname -m)"
+        exit 1
+    fi
+    echo "Download ${host_platform} ${host_cpu} static source code completely"
+}
+
+check_build_result() {
+    pushd ${install_path}
+    local target_name=${1}
+    tar -xf rust-std-nightly-${1}.tar.gz
+    local file_name="./rust-std-nightly-${1}/rust-std-${1}/lib/rustlib/${1}/lib/libstd.dylib.so"
+    llvm-strip ${file_name}
+    local file_size=$(stat -c%s ${file_name})
+    # so size should not exceed 1.2M
+    rm -rf rust-std-nightly-${1}
+    if [ $file_size -ge 1228000 ]; then
+        echo "${file_name} size exceed 1.2M, size is ${file_size}"
+        exit 1;
+    fi
+    echo "The so size is ${file_size}"
+    popd
+}
+
+export_ohos_path() {
+    if [ "${host_platform}" = "linux" ]; then
+        export PATH=${rust_tools}/clang/ohos/linux-x86_64/llvm/bin:$PATH
+        export PATH=${rust_tools}/cmake/linux-x86/bin:$PATH
+    fi
+}
+
+get_exclude_file() {
+    while read line; do
+        if [ -z "$line" ]; then
+                break
+        fi
+        # get fail repository
+        tag=$(echo $line | cut -d ']' -f 1 | tr -d '[')
+        # get fail test case
+        content=$(echo $line | cut -d ']' -f 2-)
+        case $tag in
+            "all")
+                exclude_file="$exclude_file --exclude $content"
+                ;;
+            "mobile")
+                if [ "${1}" = "mobile" ]; then
+                    exclude_file="$exclude_file --exclude $content"
+                fi
+                ;;
+            "ohos")
+                if [ "${1}" = "ohos" ]; then
+                    exclude_file="$exclude_file --exclude $content"
+                fi
+                ;;
+            *)
+                echo "Unknown tag: $tag"
+                ;;
+        esac
+    done < ${shell_path}/exclude_test.txt
+}
+
+get_test_suite() {
+    test_suite_dir=("assembly" "codegen" "codegen-units" "incremental" "mir-opt"
+                    "pretty" "run-coverage" "run-coverage-rustdoc" "run-make" "run-make-fulldeps"
+                    "run-pass-valgrind" "rustdoc" "rustdoc-js" "rustdoc-js-std"
+                    "rustdoc-json" "rustdoc-ui" "ui" "ui-fulldeps")
+    for element in "${test_suite_dir[@]}"
+    do
+        all_test_suite="${all_test_suite} tests/${element}"
+    done
+}
---
+++ rust-build/ohos_ci_build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+
+readonly shell_path=$(cd $(dirname $0); pwd)
+readonly root_build_dir="${shell_path}/../../../.."
+readonly rust_source_dir="${shell_path}/.."
+readonly rust_tools="${root_build_dir}/prebuilts"
+readonly install_path="${rust_source_dir}/build/dist"
+readonly output_install="${root_build_dir}/output/"
+readonly rust_static_dir="${root_build_dir}/rust_download"
+readonly oh_tools="${rust_tools}/ohos-sdk/linux/18/native/llvm/bin"
+readonly mingw_tools="${rust_tools}/mingw-w64/ohos/linux-x86_64/clang-mingw/bin"
+readonly musl_head_file_path="${root_build_dir}/third_party/musl/include/linux"
+readonly old_version="xxxxx"
+new_version="xxxxx"
+
+source ${shell_path}/function.sh
+
+get_new_version() {
+    pushd ${root_build_dir}/third_party/llvm-project/
+    local commit_id_full=$(git rev-parse HEAD)
+    local commit_id_short=${commit_id_full:0:10}
+    new_version="OHOS llvm-project $commit_id_short"
+    echo "New version is 'OHOS llvm-project $commit_id_short'"
+    popd
+}
+
+collect_build_result() {
+    mkdir -p ${output_install}
+    if [ "${host_platform}" = "linux" ] && [ ${host_cpu} = "x86_64" ]; then
+        cp ${install_path}/rust-nightly-x86_64-unknown-linux-gnu.tar.gz ${output_install}/
+        cp ${install_path}/rust-std-nightly-x86_64-pc-windows-gnullvm.tar.gz ${output_install}/
+        cp ${install_path}/rust-std-nightly-aarch64-unknown-linux-ohos.tar.gz ${output_install}/
+        cp ${install_path}/rust-std-nightly-armv7-unknown-linux-ohos.tar.gz ${output_install}/
+        cp ${install_path}/rust-std-nightly-x86_64-unknown-linux-ohos.tar.gz ${output_install}/
+    elif [ "${host_platform}" = "darwin" ] && [ ${host_cpu} = "x86_64" ]; then
+        cp ${install_path}/rust-nightly-x86_64-apple-darwin.tar.gz ${output_install}/
+    elif [ "${host_platform}" = "darwin" ] && [ ${host_cpu} = "arm64" ]; then
+        cp ${install_path}/rust-nightly-aarch64-apple-darwin.tar.gz ${output_install}/
+    fi
+}
+
+main() {
+    detect_platform
+    clean
+    download_rust_at_net
+    copy_config
+    update_config_clang ${oh_tools} ${mingw_tools} ${musl_head_file_path}
+    get_new_version
+    update_version
+    move_static_rust_source ${rust_static_dir} ${rust_source_dir}
+    rm -rf ${rust_source_dir}/src/llvm-project/*
+    cp -r ${root_build_dir}/third_party/llvm-project/*  ${rust_source_dir}/src/llvm-project/
+    echo "Copy the llvm source code completely"
+
+    pushd ${rust_source_dir}
+    export_ohos_path
+    echo "Building the rust toolchain begin"
+    python3 ./x.py dist
+    collect_build_result
+    if [ "${host_platform}" = "linux" ]; then
+        check_build_result aarch64-unknown-linux-ohos
+    fi
+    popd
+
+    echo "Building the rust toolchain Completed"
+}
+
+main
